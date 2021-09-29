7587013be173dab24ca4d3353a68b5b9eca13f4f
bamboguirassy <didegassama@gmail.com>
2021-09-29 13:58:52 +0000

    adding responsivity to users and shops admin table

--- resources/views/admin/shop-list.blade.php
+++ resources/views/admin/shop-list.blade.php
@@ -9,41 +9,43 @@
     <div class="card">
         <div class="card-body">
             <h3 class="card-title">Liste des boutiques sur {{config('app.name')}}...</h3>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Nom</th>
-                        <th>Email</th>
-                        <th>Téléphone</th>
-                        <th>Catégorie</th>
-                        <th>Date Création</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($shops as $shop)
-                    <tr>
-                        <td scope="row">{{$loop->index+1}}</td>
-                        <td>{{$shop->nom}}</td>
-                        <td>{{$shop->email}}</td>
-                        <td>
-                            <a href="https://wa.me/{{$shop->telephonePrimaire}}">{{$shop->telephonePrimaire}}</a>
-                        </td>
-                        <td>{{$shop->categorie->nom}}</td>
-                        <td>{{date_format($shop->created_at,'d M Y')}}</td>
-                        <td>
-                            <a class="btn btn-primary" href="{{ route('shop.home',compact('shop')) }}">
-                                <span class="mbri-preview"></span>
-                            </a>
-                            <a class="btn btn-danger" href="{{route('shop.edit', compact('shop'))}}">
-                                <span class="mbri-edit2"></span>
-                            </a>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Nom</th>
+                            <th>Email</th>
+                            <th>Téléphone</th>
+                            <th>Catégorie</th>
+                            <th>Date Création</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($shops as $shop)
+                        <tr>
+                            <td scope="row">{{$loop->index+1}}</td>
+                            <td>{{$shop->nom}}</td>
+                            <td>{{$shop->email}}</td>
+                            <td>
+                                <a href="https://wa.me/{{$shop->telephonePrimaire}}">{{$shop->telephonePrimaire}}</a>
+                            </td>
+                            <td>{{$shop->categorie->nom}}</td>
+                            <td>{{date_format($shop->created_at,'d M Y')}}</td>
+                            <td>
+                                <a style="padding: 3px; border: 1px solid gray; margin-right: 3px; background-color:#1C73BA; color: white;" href="{{ route('shop.home',compact('shop')) }}">
+                                    <span class="mbri-preview"></span>
+                                </a>
+                                <a  style="padding: 3px; border: 1px solid gray; margin-right: 3px; background-color:orange; color: white;" href="{{route('shop.edit', compact('shop'))}}">
+                                    <span class="mbri-edit2"></span>
+                                </a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </section>
--- resources/views/admin/user-list.blade.php
+++ resources/views/admin/user-list.blade.php
@@ -9,30 +9,32 @@
     <div class="card">
         <div class="card-body">
             <h3 class="card-title">Liste des users sur {{config('app.name')}}...</h3>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Nom complet</th>
-                        <th>Email</th>
-                        <th>Téléphone</th>
-                        <th>Type</th>
-                        <th>Date Création</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($users as $user)
-                    <tr>
-                        <td scope="row">{{$loop->index+1}}</td>
-                        <td>{{ $user->name }}</td>
-                        <td>{{ $user->email }}</td>
-                        <td>{{ $user->telephone }}</td>
-                        <td>{{ $user->type }}</td>
-                        <td>{{ date_format($user->created_at,'d M Y') }}</td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Nom complet</th>
+                            <th>Email</th>
+                            <th>Téléphone</th>
+                            <th>Type</th>
+                            <th>Date Création</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($users as $user)
+                        <tr>
+                            <td scope="row">{{$loop->index+1}}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->telephone }}</td>
+                            <td>{{ $user->type }}</td>
+                            <td>{{ date_format($user->created_at,'d M Y') }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </section>
