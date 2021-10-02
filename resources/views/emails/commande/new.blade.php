8680ffe6c30214ef1c5773c00369cd3340f8cc05
bamboguirassy <didegassama@gmail.com>
2021-10-02 20:04:05 +0000

    adding commande open button

--- resources/views/emails/commande/new.blade.php
+++ resources/views/emails/commande/new.blade.php
@@ -3,9 +3,21 @@
 
 Bonjour ***{{$commande->user->name}}***,
 Bienvenue chez **{{$commande->shop->nom}}**.
-Nous avons reçu avec succès votre commande numéro **{{$commande->numero}}** passée dans notre boutique.
-Nous allons vous revenir très prochainement.
-Cordialement.
+Nous avons reçu avec succès votre commande numéro **{{$commande->numero}}** passée dans notre boutique.  
+|  Produit | Image | Quantité |
+|---|---|---|
+@foreach ($commande->produits as $produit)
+|  {{$produit->produit->nom}} |   @if(count($produit->produit->images)>0) [![]({{asset('uploads/produits/images/'.$produit->produit->images[0]->nom)}} "{{$produit->produit->nom}}")]({{asset('uploads/produits/images/'.$produit->produit->images[0]->nom)}}) @endif | {{$produit->quantite}} unité(s) |
+@endforeach
+Nous serez notifiés de la suite de la commande.  
+
+Vous pouvez suivre l'évolution de la commande en cliquant sur le bouton suivant :  
+@component('mail::button', ['url' => route('shop.commande.show',['shop'=>$commande->shop,'commande'=>$commande])])
+Accèder à la commande
+@endcomponent
+
+Cordialement,  
+{{$commande->shop->nom}}
 
 ---
 **Katalog** est un produit de [Bambo GROUP](https://bambogroup.net)
